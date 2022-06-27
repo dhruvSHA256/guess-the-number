67040a272c4014d67ffb379d379932a96d1a0475
DHruv <dhruv.sha256@gmail.com>
2022-06-28 02:36:43 +0530

    added js logic

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+const setMessage = function (msg) {
+    document.querySelector(".message").textContent = msg;
+}
+
+const updateHighscore = function (score, highscore) {
+    if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
+}
+
+const eventCheck = function () {
+    const number = Number(document.querySelector(".guess").value);
+    if (!number) {
+        setMessage('⛔️ No number!');
+    }
+    else if (number === correctNumber) {
+        setMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = correctNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        updateHighscore(score, highscore);
+    }
+    else {
+
+        if (score) {
+            setMessage(number > correctNumber ? '📈 Too high!' : '📉 Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            setMessage('💥 You lost the game!');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+}
+
+const eventAgain = function () {
+    score = 20;
+    correctNumber = getRandomInt(1, 20);
+    setMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+}
+
+let correctNumber = getRandomInt(1, 20);
+let score = 20;
+let highscore = 0;
+
+document.querySelector(".check").addEventListener('click', eventCheck);
+document.querySelector('.again').addEventListener('click', eventAgain);
